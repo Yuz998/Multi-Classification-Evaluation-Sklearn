addf39fcf4789b04431a51b15af269c1172c4a97
Yuz998 <yuzhao963@gmail.com>
2020-05-25 16:23:14 +0800

    add

---
+++ MultiClassEvaluation.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import jaccard_similarity_score
+from sklearn.metrics import f1_score
+from scipy import interp
+
+def eval(y_true, y_pred, path, n_class):
+    '''
+    y_true:    The true values of multiple classifications
+    y_pred:    Predictive values of multiple classifications
+    y_score:   One hot vector of multiple classifications
+    y_one_hot: One hot vector of multiple classifications
+    return:
+            1. AUC of micro and macro
+            2. ROC curves for each classification
+            3. Average ROC curve
+            4. Average PRC curve
+            5. Confusion matrix
+            6. Classification report
+            7. F1 score
+    '''
+    np.random.seed(0)
+
+    y_score = np.zeros((len(y_pred), n_class))
+    y_one_hot = np.zeros((len(y_true), n_class))
+    for i,j in enumerate(y_pred):
+        y_score[i][j] = 1
+    for i,j in enumerate(y_true):
+        y_one_hot[i][j] = 1
+
+    # The function is called to caculate the AUC of type micro and macro
+    auc_micro = metrics.roc_auc_score(y_one_hot, y_score, average='micro')
+    auc_macro = metrics.roc_auc_score(y_one_hot, y_score, average='macro')
+    print('auc micro：', auc_micro)
+    print("auc macro: ", auc_macro)
+    
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_class):
+        fpr[i], tpr[i], _ = roc_curve(y_one_hot[:, i], y_score[:, i])
+    roc_auc[i] = metrics.auc(fpr[i], tpr[i])
+
+
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_one_hot.ravel(), y_score.ravel())
+    roc_auc["micro"] = metrics.auc(fpr["micro"], tpr["micro"])
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_class)]))
+
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_class):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    mean_tpr /= n_class
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = metrics.auc(fpr["macro"], tpr["macro"])
+
+    ## Draw ROC curves for each classification
+    lw=2
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+            label='micro-average ROC curve (area = {0:0.2f})'
+                ''.format(roc_auc["micro"]),
+            color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+            label='macro-average ROC curve (area = {0:0.2f})'
+                ''.format(roc_auc["macro"]),
+            color='navy', linestyle=':', linewidth=4)
+    for i in range(n_class):
+        fpr, tpr, thresholds = metrics.roc_curve(y_one_hot[:,i], y_score[:,i])
+        auc = metrics.auc(fpr, tpr)
+        ## Set font and character display
+        mpl.rcParams['font.sans-serif'] = u'SimHei'
+        mpl.rcParams['axes.unicode_minus'] = False
+        # FPR is the X axis,TPR is the Y axis
+        plt.plot(fpr, tpr, lw = 2, alpha = 0.7, label = u'ROC curve of class %d (AUC=%.3f)' %(i,auc))
+        plt.plot((0, 1), (0, 1), c = '#808080', lw = 1, ls = '--', alpha = 0.7)
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('FPR (False Positive Rate)', fontsize=13)
+    plt.ylabel('TPR (True Positive Rate)', fontsize=13)
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.savefig(path+"multi-class.png")
+
+    # Average ROC curve
+    plt.figure()
+    fpr1, tpr1, thresholds1 = metrics.roc_curve(y_one_hot.ravel(), y_score.ravel())
+    auc1 = metrics.auc(fpr1, tpr1)
+    plt.plot(fpr1, tpr1, lw=2, alpha=0.7, label='Area Under the Curve (area = %0.4f)'%auc1)
+    plt.xlabel('FPR (False Positive Rate)', fontsize=13)
+    plt.ylabel('TPR (True Positive Rate)', fontsize=13)
+    plt.title('ROC')
+    plt.legend(loc="lower right")
+    plt.savefig(path+'ROC.png')
+
+    ## Average PRC curve
+    plt.figure()
+    precision, recall, thresholds = precision_recall_curve(y_one_hot.ravel(), y_score.ravel())
+    precision = np.fliplr([precision])[0] 
+    recall = np.fliplr([recall])[0]
+    AUC_prec_rec = np.trapz(precision,recall)
+    print ("\nArea under Precision-Recall curve: " +str(AUC_prec_rec))
+    prec_rec_curve = plt.figure()
+    plt.plot(recall,precision,'-',label='Area Under the Curve (AUC = %0.4f)' % AUC_prec_rec)
+    plt.title('Precision - Recall curve')
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.legend(loc="lower right")
+    plt.savefig(path+"PRC.png")
+
+    ## Confusion matrix
+    confusion = confusion_matrix(y_true, y_pred)
+    print(confusion)
+
+    ## Calculate F1 score
+    F1_score_micro = f1_score(y_true, y_pred, labels=None, average='micro', sample_weight=None)
+    F1_score_macro = f1_score(y_true, y_pred, labels=None, average='micro', sample_weight=None)
+    print ("\nF1 score (F-measure) micro: " +str(F1_score_micro))
+    print ("\nF1 score (F-measure) macro: " +str(F1_score_macro))
+
+    ## Calculate classification report
+    print("\nclassification report: ")
+    classification_report = metrics.classification_report(y_true, y_pred, labels=[i for i in range(n_class)])
+    print(classification_report)
+
+    ## Save the result
+    file_perf = open(path+'performances.txt', 'w')
+    file_perf.write("micro-average ROC curve: "+str(auc_micro)
+                        +"\nmacro-average ROC curve: "+str(auc_macro)
+                        +"\nF1 score (F-measure) micro: " +str(F1_score_micro)
+                        +"\nF1 score (F-measure) macro: " +str(F1_score_macro)
+                        +"\n\nConfusion matrix:\n"
+                        +str(confusion)
+                        +"\n\nclassification_report:\n"
+                        +str(classification_report)
+                        )
+    file_perf.close()
---
+++ evaluation.py
@@ -0,0 +1,106 @@
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import jaccard_similarity_score
+from sklearn.metrics import f1_score
+from sklearn import metrics
+from scipy.ndimage.morphology import binary_erosion
+
+
+def eval(y_true, y_scores, output_folder, cutoff):
+    '''
+    y_true:    The true values of multiple classifications
+    y_scores:  Multiple classifications prediction scores
+    '''
+    y_scores = np.where(y_scores>cutoff, 1, 0)
+
+    # Area under the ROC curve
+    fpr, tpr, thresholds = roc_curve((y_true), y_scores)
+    AUC_ROC = roc_auc_score(y_true, y_scores)
+    print ("\nArea under the ROC curve: " +str(AUC_ROC))
+    fig =plt.figure()
+    plt.plot(fpr,tpr,'-',label='Area Under the Curve (AUC = %0.4f)' % AUC_ROC)
+    plt.title('ROC curve')
+    plt.xlabel("FPR (False Positive Rate)")
+    plt.ylabel("TPR (True Positive Rate)")
+    plt.legend(loc="lower right")
+    plt.savefig(output_folder+"ROC.png")
+
+    # Precision-recall curve
+    precision, recall, thresholds = precision_recall_curve(y_true, y_scores)
+    precision = np.fliplr([precision])[0] 
+    recall = np.fliplr([recall])[0]
+    AUC_prec_rec = np.trapz(precision,recall)
+    print ("\nArea under Precision-Recall curve: " +str(AUC_prec_rec))
+    prec_rec_curve = plt.figure()
+    plt.plot(recall,precision,'-',label='Area Under the Curve (AUC = %0.4f)' % AUC_prec_rec)
+    plt.title('Precision - Recall curve')
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.legend(loc="lower right")
+    plt.savefig(output_folder+"Precision_recall.png")
+
+    ## Calculate classification report
+    print("\nclassification report: ")
+    classification_report = metrics.classification_report(y_true, y_scores, labels=[0,1])
+    print(classification_report)
+
+    #Confusion matrix
+    threshold_confusion = cutoff
+    print ("\nConfusion matrix:  Custom threshold (for positive) of " +str(threshold_confusion))
+    y_pred = np.empty((y_scores.shape[0]))
+    for i in range(y_scores.shape[0]):
+        if y_scores[i]>=threshold_confusion:
+            y_pred[i]=1
+        else:
+            y_pred[i]=0
+    confusion = confusion_matrix(y_true, y_pred)
+    print (confusion)
+    accuracy = 0
+    if float(np.sum(confusion))!=0:
+        accuracy = float(confusion[0,0]+confusion[1,1])/float(np.sum(confusion))
+    print ("Global Accuracy: " +str(accuracy))
+    specificity = 0
+    if float(confusion[0,0]+confusion[0,1])!=0:
+        specificity = float(confusion[0,0])/float(confusion[0,0]+confusion[0,1])
+    print ("Specificity: " +str(specificity))
+    sensitivity = 0
+    if float(confusion[1,1]+confusion[1,0])!=0:
+        sensitivity = float(confusion[1,1])/float(confusion[1,1]+confusion[1,0])
+    print ("Sensitivity: " +str(sensitivity))
+    precision = 0
+    if float(confusion[1,1]+confusion[0,1])!=0:
+        precision = float(confusion[1,1])/float(confusion[1,1]+confusion[0,1])
+    print ("Precision: " +str(precision))
+
+    #Jaccard similarity index
+    jaccard_index = jaccard_similarity_score(y_true, y_pred, normalize=True)
+    print ("\nJaccard similarity score: " +str(jaccard_index))
+
+    # F1 score
+    F1_score = f1_score(y_true, y_pred, labels=None, average='binary', sample_weight=None)
+    print ("\nF1 score (F-measure): " +str(F1_score))
+
+    # Save the results
+    file_perf = open(output_folder+'performances.txt', 'w')
+    file_perf.write("Area under the ROC curve: "+str(AUC_ROC)
+                    + "\nArea under Precision-Recall curve: " +str(AUC_prec_rec)
+                    + "\nJaccard similarity score: " +str(jaccard_index)
+                    + "\nF1 score (F-measure): " +str(F1_score)
+                    +"\n\nConfusion matrix:"
+                    +str(confusion)
+                    +"\nClassification report:"
+                    +str(classification_report)
+                    +"\nACCURACY: " +str(accuracy)
+                    +"\nSENSITIVITY: " +str(sensitivity)
+                    +"\nSPECIFICITY: " +str(specificity)
+                    +"\nPRECISION: " +str(precision)
+                    )
+    file_perf.close()
+
